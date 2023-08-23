edd46454acc2f7e95e2e08b584312a717801b267
lkook <lucasheinrich.kook@uzh.ch>
2023-08-23 16:44:49 +0200

    refactor

--- inst/code/intro-binary.R
+++ inst/code/intro-binary.R
@@ -11,26 +11,6 @@ library("pROC")
 library("tidyverse")
 theme_set(theme_bw())
 
-# FUNs --------------------------------------------------------------------
-
-fmt <- function(x, break.eps = 1e-04, break.middle = 0.01, na.form = "NA",  ...) {
-  format1Pval <- function(pv) {
-    if (is.na(pv)) {
-      na.form
-    }
-    else if (pv < break.eps) {
-      paste("<", format(break.eps, scientific = FALSE))
-    }
-    else {
-      largep <- pv >= break.middle
-      format(pv, digits = 1 + largep, nsmall = 1 + largep,
-             scientific = FALSE, ...)
-    }
-  }
-  vapply(X = x, FUN = format1Pval, FUN.VALUE = "", USE.NAMES = TRUE)
-}
-
-
 # DGP ---------------------------------------------------------------------
 
 dgp <- function(n = 5e2) {
