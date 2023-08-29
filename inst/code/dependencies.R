1f638c8c1fbb90090c053ad858b0b492686f29cd
lkook <lucasheinrich.kook@uzh.ch>
2023-08-29 11:06:00 +0200

    deps

--- inst/code/dependencies.R
+++ inst/code/dependencies.R
@@ -4,15 +4,15 @@
 ### Install remotes for installing all other packages
 install.packages("remotes", repos = "https://cloud.r-project.org")
 
-### Install tramicp and helpers locally
-remotes::install_local(dependencies = TRUE)
-remotes::install_local("inst/helpers", dependencies = TRUE)
-
 ### Install RCIT from GitHub
 remotes::install_github("ericstrobl/RCIT")
 
 ### Install all other packages from CRAN
 pkgs <- c("MASS", "SimDesign", "coin", "colorspace", "cotram", "future.apply",
-          "generalhoslem", "glmnet", "multcomp", "pROC", "patchwork", "tramME",
-          "tramnet", "tidyverse")
+          "generalhoslem", "glmnet", "multcomp", "pROC", "patchwork", "tidyverse",
+          "tram", "mlt", "ranger", "beepr", "pcalg")
 remotes::install_cran(pkgs)
+
+### Install tramicp and helpers locally
+remotes::install_local()
+remotes::install_local("inst/helpers")
